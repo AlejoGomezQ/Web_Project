6c0847df206ecfc1bd93eefb8ef6b0479979bc74
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-07-23 18:14:23 -0500

    fix: variable changed

--- modules/fetchVideo.js
+++ modules/fetchVideo.js
@@ -1,10 +1,10 @@
-const apiKey = "AIzaSyCAnMEGYNe46hK1zOC-4wvRWFhOFFqmT4A";
+const X = "AIzaSyCAnMEGYNe46hK1zOC-4wvRWFhOFFqmT4A";
 const channelId = "UCLjhW-Y-i4gEWuJaScv5Ufg";
 
 export async function fetchLatestVideo() {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&order=date&part=snippet&type=video&maxResults=1`
+      `https://www.googleapis.com/youtube/v3/search?key=${X}&channelId=${channelId}&order=date&part=snippet&type=video&maxResults=1`
     );
     const data = await response.json();
     const videoId = data.items[0].id.videoId;
