d6bb28b6c40fd1fe06b94668fb5a1a12819c7e74
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-08-21 13:57:17 -0500

    chore: file system changed and vite config added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import "./styles.scss";
+
 import { fetchLatestVideo } from "./modules/fetchVideo.js";
 import { initModal } from "./modules/imageModal.js";
 import { openWp } from "./modules/openWhatsapp.js";
 import { adjustFormIframe } from "./modules/form.js";
-import { translate } from "./modules/translate.js";
+/* import { translate } from "./modules/translate.js"; */
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchLatestVideo();
@@ -13,5 +15,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
   adjustFormIframe();
 
-  translate();
+  /* translate(); */
 });
